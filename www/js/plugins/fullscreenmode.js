b7fa769e4dbe4403e5314f8488aa2c20ba463935
Johnny Watts <warhamr1488@gmail.com>
2024-07-31 17:13:22 -0400

    improves framerate and animation speed

---
+++ www/js/plugins/fullscreenmode.js
@@ -0,0 +1,9 @@
+Graphics._defaultStretchMode = function() {
+	Graphics._switchFullScreen();
+    return true;
+};
+
+SceneManager._screenWidth  = screen.width;
+SceneManager._screenHeight = screen.height;
+SceneManager._boxWidth     = screen.width;
+SceneManager._boxHeight    = screen.height;
---
+++ www/js/plugins/test.js
@@ -0,0 +1,15 @@
+//Generated using MJM's RPG Maker MV Generator Extension - its ok to delete this line
+    
+/*: 
+* @plugindesc i dont know yet
+* @author kaotickj
+* 
+* @help
+* 
+* 
+*/
+
+(function(){
+var parameters = PluginManager.parameters('test');
+
+})();
