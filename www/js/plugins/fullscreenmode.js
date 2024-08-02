044fc76ab94502943eb17efe9ff7cea9fee22fc7
Johnny Watts <warhamr1488@gmail.com>
2024-08-01 23:27:26 -0400

    updates riddler's dialog

--- www/js/plugins/fullscreenmode.js
+++
@@ -1,9 +0,0 @@
-Graphics._defaultStretchMode = function() {
-	Graphics._switchFullScreen();
-    return true;
-};
-
-SceneManager._screenWidth  = screen.width;
-SceneManager._screenHeight = screen.height;
-SceneManager._boxWidth     = screen.width;
-SceneManager._boxHeight    = screen.height;
